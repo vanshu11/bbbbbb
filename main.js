8c5ebe037de319ae742cb95123a38c6579fb7ad4
vanshu11 <87985098+vanshu11@users.noreply.github.com>
2021-08-25 20:22:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+car2_width = 120;
+car2_height = 70;
+car2_image = "car2.png";
+
+
+
+car2_x = 10;
+car2_y = 10;
+
+
+function add() {
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
+
+	ar1_imgTag = new Image(); //defining a variable with a new image
+	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
+	car1_imgTag.src = car2_image;   // load image
+
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+
+}
+
+function uploadgreencar() {
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function car2_up()
+{
+	if(car2_y >=0)
+	{
+		car2_y = car2_y - 10;
+		console.log("When up arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		 uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_down()
+{
+	if(car1_y <=500)
+	{
+		car2_y =car2_y+ 10;
+		console.log("When down arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_left()
+{
+	if(car2_x >= 0)
+	{
+		car2_x =car2_x - 10;
+		console.log("When left arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		 uploadcar1();
+		 uploadcar2();
+	}
+}
+function car2_right()
+{
+	if(car2_x <= 700)
+	{
+		car2_x =car2_x + 10;
+		console.log("When right arrow is pressed,  x = " + car2_x + " | y = " +car2_y);
+		uploadBackground();
+		uploadcar1();
+		 uploadcar2();
+   }
+}
